723e1e78af9daa5aaac0059225bae74c4ea046ea
Danish-Farooqui <danishfarooqui303@gmail.com>
2024-11-09 13:03:31 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,67 @@
+//Question No -1
+
+var favmovie = "1 Ertugrul ghazi";
+var favbook = "2 Quran MAjeed";
+var favhobby = "3 Travelling";
+
+console.log(favmovie,favbook,favhobby);
+
+//Question No -2
+
+var  iborn = "1 Saharanpur";
+var ilive = "2 Saharanpur";
+
+console.log(iborn,ilive);
+
+//Question No -3
+
+var mood = "1 Happy";
+
+console.log(mood);
+
+//Question No -4
+
+var favfruit = "1 Mango II Banana";
+var favanimal = "2 Cat";
+var favseason = "3 Winter";
+
+console.log(favfruit,favanimal,favseason);
+
+//Question No -5
+
+var favsport = "Cricket";
+
+console.log(favsport);
+
+//Question No -6
+
+var favsong = "1 ----";
+var favartist = "2 SRK";
+
+console.log(favsong,favartist);
+
+//Question No -7
+
+var siblling = 3;
+
+console.log(siblling);
+
+//Question No -8
+
+var favcuisine = "1 Anas";
+var favdrink = "2 Lassi";
+var favdessert = "3 All Like Sweet";
+
+console.log(favcuisine,favdrink,favdessert);
+
+//Question No -9
+
+var bestfriend = "Khizar";
+
+console.log(bestfriend);
+
+//Question No -10
+
+var favholiday = "Kashmir";
+
+console.log(favholiday);
